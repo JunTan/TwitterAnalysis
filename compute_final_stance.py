6d71a8a33196c5b3e2474d97b8ad977a8b0adc9b
Jun Tan <semou9325@gmail.com>
2017-04-23 12:06:27 -0700

    use hashtag in stance calculation

--- compute_final_stance.py
+++ compute_final_stance.py
@@ -1,7 +1,7 @@
 import scipy.io
 import numpy as np
 #Combine Stance
-bow_data = scipy.io.loadmat('stance.mat')
+bow_data = scipy.io.loadmat('stance_word.mat')
 bow_stance = bow_data['indiv_stance']
 bow_stance = bow_stance.flatten()
 
@@ -13,6 +13,10 @@ classifier_stance = 2*(classifier_stance-0.5)
 
 final_stance = 0.5*(bow_stance + classifier_stance)
 np.savetxt('final_stance_word.csv', final_stance, delimiter=',')
+
+bow_data = scipy.io.loadmat('stance_hashtag.mat')
+bow_stance = bow_data['indiv_stance']
+bow_stance = bow_stance.flatten()
 
 classifier_data = scipy.io.loadmat('indiv_logistic_prop_hashtag.mat')
 classifier_stance = classifier_data['prochoice_strength']
